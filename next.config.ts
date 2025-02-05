a9a28de1a210e8de59e526be74aae48988a8fcc9
ajhacker03 <ajayhacker03@gmail.com>
2025-02-06 00:37:27 +0530

    Added no cache to headers

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        // Add cache control headers to specific routes
+        source: "/(.*)", // Apply to all routes
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-store, max-age=0, must-revalidate",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
